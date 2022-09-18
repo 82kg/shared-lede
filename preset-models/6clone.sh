914b92bbc20144e487fa3164c6701304481aa693
tonySun <sun82kg@gmail.com>
2022-09-18 21:16:53 +0800

    Update 6clone.sh

--- preset-models/6clone.sh
+++ preset-models/6clone.sh
@@ -9,8 +9,8 @@ git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 rm -rf package/lean/luci-theme-argon
 mkdir -p package/myapps
 cd package/myapps
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
+#git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
